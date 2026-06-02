9d5da3b4498f49a33c765e227c01b98985e74ca8
Ashura <rakotoarisonhajatsitohaina@gmail.com>
2026-06-02 06:14:11 +0300

    Add Room import to AppDatabase.kt

--- app/src/main/java/com/autodiscipline/data/database/AppDatabase.kt
+++ app/src/main/java/com/autodiscipline/data/database/AppDatabase.kt
@@ -1,6 +1,7 @@
 package com.autodiscipline.data.database
 
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.autodiscipline.data.dao.DailyTaskDao
