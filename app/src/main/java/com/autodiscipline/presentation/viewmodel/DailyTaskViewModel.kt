00e9614a258e41d32302ac31fc8150e2ad6d6d57
Ashura <rakotoarisonhajatsitohaina@gmail.com>
2026-06-06 11:31:20 +0300

    Update DailyTaskViewModel.kt

--- app/src/main/java/com/autodiscipline/presentation/viewmodel/DailyTaskViewModel.kt
+++ app/src/main/java/com/autodiscipline/presentation/viewmodel/DailyTaskViewModel.kt
@@ -73,21 +73,21 @@ class DailyTaskViewModel @Inject constructor(
     }
 
     fun getPredefinedTasks(): List<DailyTask> {
-        return listOf(
-            DailyTask(name = "Se réveiller à l'heure choisie", order = 1),
-            DailyTask(name = "Prière du matin", order = 2),
-            DailyTask(name = "Sport (1 à 2 h)", order = 3),
-            DailyTask(name = "Douche froide", order = 4),
-            DailyTask(name = "Méditation (10 à 20 min)", order = 5),
-            DailyTask(name = "Lecture matinale (1 à 1 h 30)", order = 6),
-            DailyTask(name = "Planification de la journée", order = 7),
-            DailyTask(name = "Ne pas se masturber", order = 8),
-            DailyTask(name = "Étude concentrée (1 h 30 à 2 h)", order = 9),
-            DailyTask(name = "1 h sans Internet (12 h à 13 h)", order = 10),
-            DailyTask(name = "Révision et préparation du lendemain", order = 11),
-            DailyTask(name = "Bilan de la journée", order = 12),
-            DailyTask(name = "Prière du soir", order = 13),
-            DailyTask(name = "Pas de pornographie", order = 14)
-        )
+    return listOf(
+        DailyTask(id = 1, name = "Se réveiller à l'heure choisie", order = 1),
+        DailyTask(id = 2, name = "Prière du matin", order = 2),
+        DailyTask(id = 3, name = "Sport (1 à 2 h)", order = 3),
+        DailyTask(id = 4, name = "Douche froide", order = 4),
+        DailyTask(id = 5, name = "Méditation (10 à 20 min)", order = 5),
+        DailyTask(id = 6, name = "Lecture matinale (1 à 1 h 30)", order = 6),
+        DailyTask(id = 7, name = "Planification de la journée", order = 7),
+        DailyTask(id = 8, name = "Ne pas se masturber", order = 8),
+        DailyTask(id = 9, name = "Étude concentrée (1 h 30 à 2 h)", order = 9),
+        DailyTask(id = 10, name = "1 h sans Internet (12 h à 13 h)", order = 10),
+        DailyTask(id = 11, name = "Révision et préparation du lendemain", order = 11),
+        DailyTask(id = 12, name = "Bilan de la journée", order = 12),
+        DailyTask(id = 13, name = "Prière du soir", order = 13),
+        DailyTask(id = 14, name = "Pas de pornographie", order = 14)
+    )
     }
 }
