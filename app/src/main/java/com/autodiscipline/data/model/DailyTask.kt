3c4b104ed4639cc7d4b6f459db66faa3bdc37400
Ashura <rakotoarisonhajatsitohaina@gmail.com>
2026-06-06 11:29:39 +0300

    Update DailyTask.kt

--- app/src/main/java/com/autodiscipline/data/model/DailyTask.kt
+++ app/src/main/java/com/autodiscipline/data/model/DailyTask.kt
@@ -5,8 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "daily_tasks")
 data class DailyTask(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+    @PrimaryKey
+    val id: Int,
     val name: String,
     val order: Int,
     val isChecked: Boolean = false,
