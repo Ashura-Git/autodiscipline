6225c8eb5912eb2a0f8afe32b14376223d80261a
Ashura <rakotoarisonhajatsitohaina@gmail.com>
2026-06-07 08:43:22 +0300

    Update NavGraphs.kt

--- app/src/main/java/com/autodiscipline/presentation/navigation/NavGraphs.kt
+++ app/src/main/java/com/autodiscipline/presentation/navigation/NavGraphs.kt
@@ -2,6 +2,7 @@ package com.autodiscipline.presentation.navigation
 
 sealed class Screen(val route: String) {
     object Welcome : Screen("welcome_screen")
+    object Intro : Screen("intro_screen")
     object Home : Screen("home_screen")
     object History : Screen("history_screen")
     object Statistics : Screen("statistics_screen")
